c981c01703c9805d90af99e684a7ec4edde562fd
Steve2404 <jatsasteve@gmail.com>
2024-06-12 12:49:54 +0200

    rename functions and delete unuse

--- kommu/chat4/GUI/hook-qiskit.py
+++
@@ -1,8 +0,0 @@
-# hook-qiskit.py
-
-from PyInstaller.utils.hooks import collect_data_files, collect_submodules
-
-datas = collect_data_files('qiskit')
-hiddenimports = collect_submodules('qiskit')
-
-print(datas)
